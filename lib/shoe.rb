8939d5cb82225995cd2199a8a736b66ea9444a6d
Emilia Ziemichód <emifrompoland@gmail.com>
2019-04-07 22:15:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,15 +6,12 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-
-  def brands=(brand)
-    @brands=brand
   end
 
 end
